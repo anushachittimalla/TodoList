6b37494dba545f0c1191bca48c168d640221b396
anusha.chittimalla <anusha.chittimalla24@gmail.com>
2024-08-09 10:40:28 +0530

    change2

--- src/App.js
+++ src/App.js
@@ -6,94 +6,74 @@ function App() {
   const defaultValue = ""
   const[inputTodo, setInputTodo] = useState(defaultValue)
   const[allTasksDetails, setallTaskDetails] =useState([]);
-  const[activeTasksDetails, setActiveTaskDetails] =useState([]);
-  const[completedTasksDetails, setCompletedTaskDetails] =useState([]);
-
-  const[showAll,setShowAll] = useState(false)
-  const[showActive,setShowActive] = useState(false)
-  const[showCompleted,setShowCompleted] = useState(false)
-
-  
+  const[showTasksDetails, setShowTaskDetails] =useState([]);
+ 
   const handleChange = (e) =>{
-    setInputTodo( e.target.value)
-    //console.log(inputTodo)
-  }
-  
+    setInputTodo( e.target.value);
+  }  
   
   const handleKeyDown = (event) => {
     console.log(allTasksDetails)
     if(event.target.value?.trim() === '') return;
     if (event.key === "Enter") {
-      setallTaskDetails(allTasksDetails => [...allTasksDetails,{ value: inputTodo.trim(), isChecked: false}] );
+      const temp = allTasksDetails => [...allTasksDetails, { value: inputTodo.trim(), isCompleted: false}];
+      setShowTaskDetails(temp);
+      setallTaskDetails(temp);
       setInputTodo(defaultValue)
-      setShowAll(true)
     }
-  };  
+  };
+
   const deleteHandler = i => {
-    setallTaskDetails(allTasksDetails.filter((task, index) => index !== i));
+    const temp = allTasksDetails.filter((task, index) => index !== i);
+    setShowTaskDetails(temp);
+    setallTaskDetails(temp);
   }
   const displayAll = () => {
-    setShowAll(true)
-    setShowActive(false)
-    setShowCompleted(false)
-
+    setShowTaskDetails(allTasksDetails);
   }
+
   const displayActive = () => {
-    const activeTasks = allTasksDetails.filter((task) => task.isChecked === false)
-    setActiveTaskDetails(activeTasks);
-    setShowAll(false)
-    setShowActive(true)
-    setShowCompleted(false)
-
-
+    const activeTasks = allTasksDetails.filter((task) => task.isCompleted === false)
+    setShowTaskDetails(activeTasks);
   }
+
   const displayCompleted = () => {
-    const completedTasks = allTasksDetails.filter((task) => task.isChecked === true)
-    setCompletedTaskDetails(completedTasks);
-    setShowAll(false)
-    setShowCompleted(true)
-    setShowActive(false)
+    const completedTasks = allTasksDetails.filter((task) => task.isCompleted === true)
+    setShowTaskDetails(completedTasks);
   }
+
   const clearCompletedTodo = () => {
-    setallTaskDetails(allTasksDetails.filter((task) => task.isChecked === false))
+    const temp = allTasksDetails.filter((task) => task.isCompleted === false);
+    setallTaskDetails(temp);
+    setShowTaskDetails(temp);    
   }
+
+  const setCheckboxCheck = (e, index, each) => {
+    let newArr = [...allTasksDetails]; 
+    const temp = { value: each.value, isCompleted: e.target.checked }
+    newArr[index] = temp;
+    setallTaskDetails(newArr);
+    setShowTaskDetails(newArr)     
+  }
+
   return (
     <div className="App mt-5 mb-3">
       <h1> TODO</h1>
   
-      <input type="text" placeholder="Create a new todo" className="p-3 create-todo" onChange={(e)=>handleChange(e)} onKeyDown={handleKeyDown} value={inputTodo} /> <br/>
-      {showAll && allTasksDetails.length > 0 && allTasksDetails.map((each, i) => 
+      <input type="text" placeholder="Create a new todo" className="p-3 create-todo" onChange={handleChange} onKeyDown={handleKeyDown} value={inputTodo} /> <br/>
+      { showTasksDetails.length > 0 && showTasksDetails.map((each, i) => 
       <div className='eachtodo mt-4  pt-2' key={i}> 
-      <input type='checkbox' className='ckeckbox' onChange={(e) => { console.log(e); each.isChecked = e.target.checked}}/>
-      {each?.value} 
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" onClick={() => deleteHandler(i)} className="bi bi-x" viewBox="0 0 16 16">
-  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-</svg>
-      </div>
-      )}
-      {showActive && activeTasksDetails.length > 0 && activeTasksDetails.map((each, i) => 
-      <div className='eachtodo mt-4  pt-2' key={i}> 
-      <input type='checkbox' className='ckeckbox' onChange={(e) => { console.log(e); each.isChecked = e.target.checked}}/>
-      {each?.value} 
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" onClick={() => deleteHandler(i)} className="bi bi-x" viewBox="0 0 16 16">
-  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-</svg>
-      </div>
-      )}
-
-{showCompleted && completedTasksDetails.length > 0 && completedTasksDetails.map((each, i) => 
-      <div className='eachtodo mt-4  pt-2' key={i}> 
-      <input type='checkbox' className='ckeckbox' onChange={(e) => { console.log(e); each.isChecked = e.target.checked}}/>
-      {each?.value} 
+      <input type='checkbox' className='ckeckbox' value={each.isCompleted} onChange={(e) => setCheckboxCheck(e, i, each)}/>
+      <p  style={{
+          textDecoration: each.isCompleted ? "line-through" :  "none",
+        }}> {each?.value}  </p>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" onClick={() => deleteHandler(i)} className="bi bi-x" viewBox="0 0 16 16">
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
 </svg>
       </div>
       )}
       <div className="todo-details mt-4 pt-2">
-        {showAll && <p>{allTasksDetails.length} items left</p>}
-        {showActive && <p>{activeTasksDetails.length} items left</p>}
-        {showCompleted && <p>{completedTasksDetails.length} items left</p>}
+        { <p>{showTasksDetails.length} items left</p>}
         <div className="all-buttons"> 
           <button className="me-3 buttons" onClick={displayAll}>All</button>
           <button className="me-3 buttons" onClick={displayActive}>Active</button>
